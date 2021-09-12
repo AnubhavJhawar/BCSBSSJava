93942b8fc200745bbe92500d9d5cea3a23df1bda
AnubhavJhawar <anubhavjhawar08@gmail.com>
2021-09-12 19:43:02 +0530

    2 Questions added

---
+++ Question2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Question2{
+    public static void main(String[] args){
+       System.out.println("Enter a number");
+       Scanner anubhav =new Scanner(System.in);
+       int a;
+       a = anubhav.nextInt();
+       System.out.println("Number Entered By User");
+       System.out.println(a);
+    }
+}
